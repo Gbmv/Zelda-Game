40b59fb16077d504d8995fb476ff91ab8b1e33a7
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-04-16 17:34:45 -0400

    Update Enemy.java
    Adding the jumping atributes to the enemy =)

--- TP2/TP2/src/main/java/tpclasses/tp2/Enemy.java
+++ TP2/TP2/src/main/java/tpclasses/tp2/Enemy.java
@@ -3,22 +3,34 @@ package tpclasses.tp2;
 public class Enemy extends Characters{
     // Atributes
     int gold;
+    double jumpSpeed;
+
 
     // Constructor
-    public void Enemy(int gold){
-//       super();
-        this.gold = gold;
-    }
 
+    public Enemy() {
+        super(500, 120,
+                "file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\enemy_image.png",
+                100,  300);
+        this.jumpSpeed = -300;
+    }
 
     // Getters
     public int getGold() {
         return gold;
     }
 
+    public double getjumpSpeed(){
+        return jumpSpeed;
+    }
+
     // Setters
 
     public void setGold(int gold) {
         this.gold = gold;
+    }
+
+    public void setJumpSpeed(double jumpSpeed) {
+        this.jumpSpeed = jumpSpeed;
     }
 }

566194884069913571284fd16b510cb0e8683767
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-04-20 16:23:42 -0400

    Update HeroBodyToBody.java

--- TP2/TP2/src/main/java/tpclasses/tp2/HeroBodyToBody.java
+++ TP2/TP2/src/main/java/tpclasses/tp2/HeroBodyToBody.java
@@ -1,4 +1,26 @@
 package tpclasses.tp2;
 
-public class HeroBodyToBody extends Hero{
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.List;
+
+public class HeroBodyToBody extends Characters{
+
+    public HeroBodyToBody() {
+        super(50, "file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\heroSthy.webp",
+                100, 200);
+    }
+    public void createHeroBody(Pane root , List<ImageView> herosBody , double width, double height){
+        Image heroBody = new Image("file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\heroBody.png");
+        ImageView heroBodyView = new ImageView(heroBody);
+        heroBodyView.setFitWidth(100);
+        heroBodyView.setFitHeight(100);
+        heroBodyView.setTranslateX(Math.random()*width+width);
+        heroBodyView.setLayoutY( Math.random() *(height - 40));
+        herosBody.add(heroBodyView);
+        root.getChildren().add(heroBodyView);
+    }
 }
+

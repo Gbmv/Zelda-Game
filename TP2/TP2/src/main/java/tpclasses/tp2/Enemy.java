ec1a2418748ec99717570565767bb49f3f2df700
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-04-20 16:23:16 -0400

    Update Enemy.java

--- TP2/TP2/src/main/java/tpclasses/tp2/Enemy.java
+++ TP2/TP2/src/main/java/tpclasses/tp2/Enemy.java
@@ -4,15 +4,19 @@ public class Enemy extends Characters{
     // Atributes
     int gold;
     double jumpSpeed;
+    double life;
+    int gravity;
+
 
 
     // Constructor
 
     public Enemy() {
-        super(500, 120,
-                "file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\enemy_image.png",
-                100,  300);
+        super(120, "file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\enemy_image.png"
+                ,100 ,300);
         this.jumpSpeed = -300;
+        this.life = 1000;
+        this.gravity = 500;
     }
 
     // Getters
@@ -20,10 +24,17 @@ public class Enemy extends Characters{
         return gold;
     }
 
-    public double getjumpSpeed(){
+    public double getJumpSpeed() {
         return jumpSpeed;
     }
 
+    public double getLife(){
+        return life;
+    }
+
+    public int getGravity() {
+        return gravity;
+    }
     // Setters
 
     public void setGold(int gold) {
@@ -33,4 +44,13 @@ public class Enemy extends Characters{
     public void setJumpSpeed(double jumpSpeed) {
         this.jumpSpeed = jumpSpeed;
     }
+
+    public void setLife(double life) {
+        this.life = life;
+    }
+
+    public void setGravity(int gravity) {
+        this.gravity = gravity;
+    }
 }
+

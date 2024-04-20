ba56a66f32e3ca68a7cfc143bad201e2b65f8f94
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-04-20 16:24:17 -0400

    Update HeroTank.java

--- TP2/TP2/src/main/java/tpclasses/tp2/HeroTank.java
+++ TP2/TP2/src/main/java/tpclasses/tp2/HeroTank.java
@@ -1,4 +1,9 @@
 package tpclasses.tp2;
 
-public class HeroTank extends Hero{
+public class HeroTank extends Characters{
+    public HeroTank() {
+        super(50, "file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\heroSthy.webp",
+                100, 200);
+    }
 }
+

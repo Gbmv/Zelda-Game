000e48eede15b6d5a8479f122cf6a49cd8995626
Gbmv <115672461+Gbmv@users.noreply.github.com>
2024-04-20 16:22:37 -0400

    Update Viewer.java
    Adding the creation of the some heros and the coins.But don't know why the problem with the location of the spawn

--- TP2/TP2/src/main/java/tpclasses/tp2/Viewer.java
+++ TP2/TP2/src/main/java/tpclasses/tp2/Viewer.java
@@ -14,6 +14,7 @@ import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Viewer extends Application {
 
@@ -34,10 +35,13 @@ public class Viewer extends Application {
         Scene scene = new Scene(root, width, height);
         Canvas canvas = new Canvas(width,height);
         GraphicsContext context = canvas.getGraphicsContext2D();
+        Random random = new Random();
 
 
 
-    //---------------------------------------------- Background ---------------------------------------------------//
+
+
+        //---------------------------------------------- Background ---------------------------------------------------//
 
         // Load background images
         Image backgroundImage = new Image("file:///C:/Users/gabri/Desktop/UDEM/Hiver-2024/IFT-1025/TP2/TP2/bg.png");
@@ -80,41 +84,96 @@ public class Viewer extends Application {
         translationImage2.setCycleCount(TranslateTransition.INDEFINITE);
         // ----------------------------------------------------------------------------------------------------------//
 
+        List<ImageView> herosTank = new ArrayList<>();
+        List<ImageView> herosBody = new ArrayList<>();
+        List<ImageView> herosSthy = new ArrayList<>();
         List<ImageView> coins = new ArrayList<>();
 
+        HeroStealthy heroStealthy = new HeroStealthy();
+        HeroBodyToBody heroBodyToBody = new HeroBodyToBody();
+
         AnimationTimer timer = new AnimationTimer() {
-            private long lastCoinTime = 0;
+            private long lastUsageTime = 0;
 
             private long lastTime = System.nanoTime();
             double speed_y = 0;
 
             final double gravity = enemy.getGravity();
-            final double backgroundSpeed = 50;
 
+            private double timerHero;
+            private double timerCoin;
+            final double backgroundSpeed = 50;
             @Override
             public void handle(long now) {
                 double deltaTime = (now - lastTime) * 1e-9;
-
+                timerHero+=deltaTime;
+                timerCoin+=deltaTime;
                 lastTime = now;
 
-                if ((now - lastCoinTime )/1e9 >= 2) {
+
+            //-------------------------Implementation of the heros----------------------------------------------------//
+//                System.out.println(timerHero);
+                if ((timerHero >= 3)) {
+//                     Gives a random number between 0 and 2
+                    int randomChoice = random.nextInt(3);
+
+                    if(randomChoice == 0 ){
+                        heroStealthy.createHeroStealhy(root, herosSthy, width);
+                    } else if (randomChoice == 1) {
+                        heroBodyToBody.createHeroBody(root,herosBody,width,height);
+                    }
+
+
+                    timerHero = 0;
+                }
+
+
+                for(ImageView heroSthyViwer: herosSthy ){
+                    double newX = heroSthyViwer.getTranslateX()-backgroundSpeed * deltaTime;
+                    heroSthyViwer.setTranslateX(newX);
+
+                    if(newX <-width){
+                        root.getChildren().remove(heroSthyViwer);
+                        herosSthy.remove(heroSthyViwer);
+                        break;
+                    }
+                }
+
+                for(ImageView heroBodyView: herosBody ){
+                    double newX = heroBodyView.getTranslateX()-backgroundSpeed * deltaTime;
+                    heroBodyView.setTranslateX(newX);
+
+                    if(newX <-width){
+                        root.getChildren().remove(heroBodyView);
+                        herosBody.remove(heroBodyView);
+                        break;
+                    }
+                }
+
+
+            //----------------------------Implementation of the coins------------------------------------------------//
+
+
+                if (timerCoin >= 2) {
                     createCoin(root, width, height, coins);
-                    lastCoinTime = now;
+                    timerCoin  = 0;
                 }
 
                 // Animate the movement of existing coins
                 for (ImageView coinView : coins) {
-                    double deltaX = -2; // Movement speed
-                    double newX = coinView.getTranslateX() + deltaX;
+                    double newX = coinView.getTranslateX() - backgroundSpeed * deltaTime;
                     coinView.setTranslateX(newX);
 
+
                     // Remove coin if it goes out of screen
-                    if (newX < -width) {
+                    if (newX < -width ) {
                         root.getChildren().remove(coinView);
                         coins.remove(coinView);
                         break; // Break loop to avoid ConcurrentModificationException
                     }
+
                 }
+                //----------------------------------------------------------------------------------------------------//
 
 
                 // Defines the translation in the background
@@ -128,12 +187,14 @@ public class Viewer extends Application {
                     backgroundViewer2.setTranslateX(width);
                 }
 
+
+
                 // -------------------------Implementations of the space bar  ------------------------------------//
 
                 speed_y += gravity * deltaTime; // gravity accelertion
                 double newY = enemyView.getLayoutY() + speed_y * deltaTime; // new Y position
 
-                // Verify if the enemy it's in the floor
+                // Verify if the enemy it not in the floor
                 if (newY >= enemy.getPositionY()) {
                     newY = enemy.getPositionY();
                 }
@@ -147,6 +208,10 @@ public class Viewer extends Application {
                         speed_y = enemy.getJumpSpeed(); // The speed of the jump
                     }
                 });
+                //---------------------------------------------------------------------------------------------------//
+
+
+
                 // Clean the context
                 context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
@@ -166,10 +231,70 @@ private void createCoin(Pane root, double width, double height, List<ImageView> 
     ImageView coinView = new ImageView(coin);
     // Coin position outside the screen
     coinView.setTranslateX(Math.random()* width + width);
-    coinView.setTranslateY( Math.random() *(height - 40));
     coinView.setFitWidth(50);
     coinView.setFitHeight(50);
+    // Why error with where the image is spawed
+    coinView.setTranslateY( Math.random() * (height - coinView.getFitHeight()));
     root.getChildren().add(coinView);
     coins.add(coinView);
 }
+//    public void createHeroTank(Pane root, List<ImageView> herosTank , double width) {
+//        double minX = -30; // Limite mínimo de X
+//        double maxX = 30; // Limite máximo de X
+//        double minY = -30; // Limite mínimo de Y
+//        double maxY = 30;
+//        Image heroTank = new Image("file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\heroTank.png");
+//        ImageView heroTankViewer = new ImageView(heroTank);
+//        heroTankViewer.setFitHeight(100);
+//        heroTankViewer.setFitWidth(100);
+//
+//        // Define uma posição inicial aleatória dentro do limite de X e Y
+//        double initialX = Math.random() * (maxX - minX) + minX;
+//        double initialY = Math.random() * (maxY - minY) + minY;
+//        heroTankViewer.setTranslateX(initialX);
+//        heroTankViewer.setTranslateY(initialY);
+//
+//        // Define a animação de teletransporte
+//        TranslateTransition teleportation = new TranslateTransition(Duration.seconds(0.5), heroTankViewer);
+//        teleportation.setByX(Math.random() * (maxX - minX) + minX);
+//        teleportation.setByY(Math.random() * (maxY - minY) + minY);
+//        teleportation.setInterpolator(Interpolator.LINEAR);
+//        teleportation.setAutoReverse(true);
+//        teleportation.setCycleCount(TranslateTransition.INDEFINITE);
+//        teleportation.play();
+//
+//        herosTank.add(heroTankViewer);
+//        root.getChildren().add(heroTankViewer);
+//    }
 }
+
+//    public void createHeroStealhy(Pane root , List<ImageView> herosSthy , double width){
+//        Image heroSthy = new Image("file:///C:\\Users\\gabri\\Desktop\\UDEM\\Hiver-2024\\IFT-1025\\TP2\\TP2\\heroSthy.png");
+//        ImageView heroSthyViwer = new ImageView(heroSthy);
+//        heroSthyViwer.setFitHeight(100);
+//        heroSthyViwer.setFitWidth(100);
+//        heroSthyViwer.setTranslateX(width);
+//        heroSthyViwer.setTranslateY(50);
+//        herosSthy.add(heroSthyViwer);
+//
+//        TranslateTransition translationHeroSthy = new TranslateTransition(Duration.seconds(3),heroSthyViwer);
+//        translationHeroSthy.setFromY(50);
+//        translationHeroSthy.setToY(150);
+//        translationHeroSthy.setCycleCount(TranslateTransition.INDEFINITE);
+//        translationHeroSthy.setAutoReverse(true);
+//        root.getChildren().add(heroSthyViwer);
+//        translationHeroSthy.play();
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
